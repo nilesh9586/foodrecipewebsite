fe7f4f099aa1a1cfcb2872da37b995fd3ce57910
nilesh9586 <nilesh16mcg34@gmail.com>
2020-12-15 15:29:13 +0530

    Updates

---
+++ precache-manifest.0db4014c9504f5bb551f87db7146464f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "36dc5fb536db4e51bac6",
+    "url": "/foodrecipewebsite/static/css/main.145d46e3.chunk.css"
+  },
+  {
+    "revision": "36dc5fb536db4e51bac6",
+    "url": "/foodrecipewebsite/static/js/main.ebc07291.chunk.js"
+  },
+  {
+    "revision": "37b0f69c2823926c2a39",
+    "url": "/foodrecipewebsite/static/js/runtime~main.d2b502f8.js"
+  },
+  {
+    "revision": "7a2b4fce4071b2916c43",
+    "url": "/foodrecipewebsite/static/css/2.f9c6d5ad.chunk.css"
+  },
+  {
+    "revision": "7a2b4fce4071b2916c43",
+    "url": "/foodrecipewebsite/static/js/2.cfce6434.chunk.js"
+  },
+  {
+    "revision": "79f715c37dc23f9cfdf5f6b5bd34d629",
+    "url": "/foodrecipewebsite/index.html"
+  }
+];
